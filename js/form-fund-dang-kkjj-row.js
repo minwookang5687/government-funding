a8db9d8da53d0c9199c2263a127fc0dd18689fb6
minwookang5687 <softman007@naver.com>
2025-10-22 17:51:28 +0900

    ㄱ
    ㄱ

--- js/form-fund-dang-kkjj-row.js
+++ js/form-fund-dang-kkjj-row.js
@@ -8,7 +8,7 @@ form.innerHTML = `
       <p class="leadform__count" style="font-size: 18px;">오늘 최대한도&최저금리<br>무료진단 받으신 대표님
         <strong id="today-count">0</strong>명
       </p>
-      <h2 id="leadform-title" class="leadform__title">2025년 하반기<br>경기도 정책자금 컨설팅<br>무료상담 신청</h2>
+      <h2 id="leadform-title" class="leadform__title">2025년 하반기<br>경기도 제조업<br>정책자금 컨설팅<br>무료상담 신청</h2>
       <p class="leadform__desc" style="font-size: 18px;">
         오랜 경력을 가진 전문 컨설턴트가<br>
         대표님 기업에 필요한 <span style="color: #ffe600;"><b>최대한도·최저금리 조건</b></span>을 비교 분석해<br>
